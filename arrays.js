2aafc9024d966e5f5f84efacf93939edb8d8bf7b
Ay Has <ahashim983@yahoo.com>
2019-03-17 16:01:09 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(arr, elem){
+  return [elem,...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem){
+  return arr.unshift(elem)
+}
+
+function addElementToEndOfArray(arr, elem){
+  return [...arr, elem]
+}
+
+function destructivelyAddElementToEndOfArray(arr, elem){
+  return arr.push(elem)
+}
+
+function accessElementInArray(arr, index){
+  return arr[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  return arr.unshift()
+}
+
+function removeElementFromBeginningOfArray(arr){
+  return arr.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  return arr.pop()
+}
+
+function removeElementFromEndOfArray(arr){
+  return arr.slice(-1)
+}
